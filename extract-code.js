fed93d387689141b46acb10382eaf86956a147ec
Momo707577045 <707577045@qq.com>
2020-09-29 19:17:28 +0800

    # 添加视频提取核心代码

---
+++ extract-code.js
@@ -0,0 +1,95 @@
+try {
+  let _sourceBufferList = []
+  let $btnDownload = document.createElement('div')
+  let $downloadNum = document.createElement('div')
+
+  // 下载资源
+  function _download () {
+    _sourceBufferList.forEach((target) => {
+      const mime = target.mime.split(';')[0]
+      const type = mime.split('/')[1]
+      const fileBlob = new Blob(target.bufferList, { type: mime }) // 创建一个Blob对象，并设置文件的 MIME 类型
+      const a = document.createElement('a')
+      a.download = `${document.title}.${type}`
+      a.href = URL.createObjectURL(fileBlob)
+      a.style.display = 'none'
+      document.body.appendChild(a)
+      a.click()
+      a.remove()
+    })
+  }
+
+  // 监听资源全部录取成功
+  let _endOfStream = window.MediaSource.prototype.endOfStream
+  window.MediaSource.prototype.endOfStream = function () {
+    alert('资源全部捕获成功，即将下载！')
+    _download()
+    _endOfStream.call(this)
+  }
+
+  // 录取资源
+  let _addSourceBuffer = window.MediaSource.prototype.addSourceBuffer
+  window.MediaSource.prototype.addSourceBuffer = function (mime) {
+    console.log(mime)
+    let sourceBuffer = _addSourceBuffer.call(this, mime)
+    let _append = sourceBuffer.appendBuffer
+    let bufferList = []
+    _sourceBufferList.push({
+      mime,
+      bufferList,
+    })
+    sourceBuffer.appendBuffer = function (buffer) {
+      $downloadNum.innerHTML = `已捕获 ${_sourceBufferList[0].bufferList.length} 个片段`
+      bufferList.push(buffer)
+      _append.call(this, buffer)
+    }
+    return sourceBuffer
+  }
+
+  // 添加操作的 dom
+  function _appendDom () {
+    $downloadNum.innerHTML = '已捕获 0 个片段'
+    $btnDownload.innerHTML = '下载已捕获片段'
+    $downloadNum.style = `
+   position: fixed;
+  top: 50px;
+  right: 50px;
+  height: 40px;
+  padding: 0 20px;
+  z-index: 9999;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 40px;
+  text-align: center;
+  border-radius: 4px;
+  background-color: #3498db;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
+  `
+    $btnDownload.style = `
+    position: fixed;
+  top: 100px;
+  right: 50px;
+  height: 40px;
+  padding: 0 20px;
+  z-index: 9999;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 40px;
+  text-align: center;
+  border-radius: 4px;
+  background-color: #3498db;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
+  `
+    $btnDownload.addEventListener('click', _download)
+    document.getElementsByTagName('html')[0].insertBefore($downloadNum, document.getElementsByTagName('head')[0]);
+    document.getElementsByTagName('html')[0].insertBefore($btnDownload, document.getElementsByTagName('head')[0]);
+  }
+
+  _appendDom()
+} catch (e) {
+
+}
--- todo.md
+++ todo.md
@@ -1,16 +1,29 @@
-<!--待完成事项-->
-### 通过重写 URL.createObjectURL 获取真实播放地址
-### 从 video 标签底层截取视频资源
+> 资源
+- https://youku.cdn4-okzy.com/20200819/9699_b5f285de/1000k/hls/index.m3u8
+
+
+> 待完成
+- 音频，视频融合
+- 普通 mp4 播放
+- 文章
 
 
 
-<!--已完成事项-->
-### 2020-07-24
-# 阅读 hls.js 源码
-# 了解 video 导入视频资源的方式
-# 相关 api 及对象
-- MediaSource
-- SourceBuffer
+> 已完成
+### 2020-09-29
+- 播放器
+- 加速播放
+- iframe 注入
+- 自测
+  - 爱奇艺
+  - 学习网站
+  - BiliBili
 
 
+### 2020-09-28
+- 逻辑实现
+- UI 实现
+- 数据获取方式，复写 appendBuffer
+- 使用实例
+- 监听停止
 
