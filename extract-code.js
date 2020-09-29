92502f76513e9a158ec795c321a7277c02ec040d
Momo707577045 <707577045@qq.com>
2020-09-29 19:51:03 +0800

    # 添加十倍速捕获功能

--- extract-code.js
+++ extract-code.js
@@ -1,7 +1,18 @@
-try {
+(function () {
+
   let _sourceBufferList = []
   let $btnDownload = document.createElement('div')
   let $downloadNum = document.createElement('div')
+  let $tenRate = document.createElement('div') // 十倍速播放
+
+  // 十倍速播放
+  function _tenRatePlay () {
+    let $domList = document.getElementsByTagName('video')
+    for (let i = 0, length = $domList.length; i < length; i++) {
+      const $dom = $domList[i]
+      $dom.playbackRate = 10
+    }
+  }
 
   // 下载资源
   function _download () {
@@ -48,48 +59,35 @@ try {
 
   // 添加操作的 dom
   function _appendDom () {
+    const baseStyle = `
+      position: fixed;
+      top: 50px;
+      right: 50px;
+      height: 40px;
+      padding: 0 20px;
+      z-index: 9999;
+      color: white;
+      cursor: pointer;
+      font-size: 16px;
+      font-weight: bold;
+      line-height: 40px;
+      text-align: center;
+      border-radius: 4px;
+      background-color: #3498db;
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
+    `
+    $tenRate.innerHTML = '十倍速捕获'
     $downloadNum.innerHTML = '已捕获 0 个片段'
     $btnDownload.innerHTML = '下载已捕获片段'
-    $downloadNum.style = `
-   position: fixed;
-  top: 50px;
-  right: 50px;
-  height: 40px;
-  padding: 0 20px;
-  z-index: 9999;
-  color: white;
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
-  line-height: 40px;
-  text-align: center;
-  border-radius: 4px;
-  background-color: #3498db;
-  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
-  `
-    $btnDownload.style = `
-    position: fixed;
-  top: 100px;
-  right: 50px;
-  height: 40px;
-  padding: 0 20px;
-  z-index: 9999;
-  color: white;
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
-  line-height: 40px;
-  text-align: center;
-  border-radius: 4px;
-  background-color: #3498db;
-  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
-  `
+    $tenRate.style = baseStyle + `top: 150px;`
+    $btnDownload.style = baseStyle + `top: 100px;`
+    $downloadNum.style = baseStyle
     $btnDownload.addEventListener('click', _download)
+    $tenRate.addEventListener('click', _tenRatePlay)
+    document.getElementsByTagName('html')[0].insertBefore($tenRate, document.getElementsByTagName('head')[0]);
     document.getElementsByTagName('html')[0].insertBefore($downloadNum, document.getElementsByTagName('head')[0]);
     document.getElementsByTagName('html')[0].insertBefore($btnDownload, document.getElementsByTagName('head')[0]);
   }
 
   _appendDom()
-} catch (e) {
-
-}
+})()
